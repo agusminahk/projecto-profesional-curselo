4632afac64a909307d559f14a8ce05ac1865f611
PatoAnastacio207 <patricioanastacio01@gmail.com>
2021-11-24 18:17:40 -0300

    profile view

--- client/src/views/App.jsx
+++ client/src/views/App.jsx
@@ -11,15 +11,17 @@ import { AdminHome } from "./admin/AdminHome"
 import { NavbarAdmin } from '../components/admin/NavbarAdmin';
 import { ClientsView } from "./admin/ClientsView"
 import { NewClient } from './admin/NewClient';
+import { Profile } from './admin/Profile';
 
 const App = () => {
     return (
         <div style={{backgroundColor: "#FFF", height: "100vh"}}>
             <NavbarAdmin />
             <Routes>
-                <Route exact path="/" element={<AdminHome />}/>
-                <Route exact path="/clientes" element={<ClientsView />}/>
-                <Route exact path="/nuevousuario" element={<NewClient />}/>
+                <Route exact path="/" element={<AdminHome />} />
+                <Route exact path="/clientes" element={<ClientsView />} />
+                <Route exact path="/nuevousuario" element={<NewClient />} />
+                <Route exact path="/perfil" element={<Profile/>} />
             </Routes>
         </div>
     )
