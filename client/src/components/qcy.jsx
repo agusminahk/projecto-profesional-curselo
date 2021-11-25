a0ebb39d0a7b37ac8d26bdfcc023855b778b53e6
leancaiguara <leancaiguara@gmail.com>
2021-11-25 11:10:41 -0300

    ajustes admin-vista

---
+++ client/src/components/qcy.jsx
@@ -0,0 +1,10 @@
+import { Text, Box, Button } from "@chakra-ui/react";
+
+export const Vamo = () => {
+  return (
+    <Box>
+      <Button>Personalizar</Button>
+      <Button>Editar perfil</Button>
+    </Box>
+  );
+};
--- client/src/views/App.jsx
+++ client/src/views/App.jsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-    useRouteMatch,
-    useParams
-  } from "react-router-dom";
-import { AdminHome } from "./admin/AdminHome"
-import { NavbarAdmin } from '../components/admin/NavbarAdmin';
-import { ClientsView } from "./admin/ClientsView"
-import { NewClient } from './admin/NewClient';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link, useRouteMatch, useParams } from "react-router-dom";
+import { AdminHome } from "./admin/AdminHome";
+import { NavbarAdmin } from "../components/admin/NavbarAdmin";
+import { ClientsView } from "./admin/ClientsView";
+import { NewClient } from "./admin/NewClient";
+import { Settings } from "./admin/Settings";
 
 const App = () => {
-    return (
-        <div style={{backgroundColor: "#FFF", height: "100vh"}}>
-            <NavbarAdmin />
-            <Routes>
-                <Route exact path="/" element={<AdminHome />}/>
-                <Route exact path="/clientes" element={<ClientsView />}/>
-                <Route exact path="/nuevousuario" element={<NewClient />}/>
-            </Routes>
-        </div>
-    )
+  return (
+    <div style={{ backgroundColor: "#FFF", height: "100vh" }}>
+      <NavbarAdmin />
+      <Routes>
+        <Route exact path="/" element={<AdminHome />} />
+        <Route exact path="/ajustes" element={<Settings />} />
+        <Route exact path="/clientes" element={<ClientsView />} />
+        <Route exact path="/nuevousuario" element={<NewClient />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
