c3b3229b92fbf7ebd1d99f2d1c3a32aa082c903b
MateoFMoreno <mateo.moreno999@gmail.com>
2021-11-30 17:17:43 -0300

    add routes subcategory

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,12 +1,17 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 const verify = (req, res, next) => {
-    const sessionCookie = req.cookies.session || '';
+    const sessionCookie = req.cookies.session || "";
     admin
         .auth()
         .verifySessionCookie(sessionCookie, true)
-        .then(({ email }) => (email ? next() : res.status(401).send('UNAUTHORIZED')))
-        .catch((e) => res.status(401).send('UNAUTHORIZED'));
+        .then(({ email }) => (email ? next() : res.status(401).send("UNAUTHORIZED")))
+        .catch((e) => res.status(401).send("UNAUTHORIZED"));
 };
 
-module.exports = verify;
+const checkAuthAdmin = (req, res, next) => (req.cookies.user.role === "admin" ? next() : res.status(401).send("unauthorized"));
+
+const checkAuthSuperAdmin = (req, res, next) =>
+    req.cookies.user.role === "superAdmin" ? next() : res.status(401).send("unauthorized");
+
+module.exports = { verify, checkAuthAdmin, checkAuthSuperAdmin };
