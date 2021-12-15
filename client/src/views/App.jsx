5451817f6f5ae0d67ff5b90ed3ee01ac39e23f64
PatoAnastacio207 <patricioanastacio01@gmail.com>
2021-12-15 11:03:27 -0300

    frontend menu

--- client/src/views/App.jsx
+++ client/src/views/App.jsx
@@ -20,6 +20,7 @@ import { Basket } from "./Basket";
 import { EditRestaurant } from "./owner/EditRestaurant";
 import { CreateRestaurant } from "./owner/CreateRestaurant";
 import { StockProd } from "./admin/StockProd";
+import { Menu } from "./Menu"
 import { useSelector, useDispatch } from "react-redux";
 import { setUser } from "../state/userSlice";
 import { setRestaurant } from "../state/restaurantSlice";
@@ -95,7 +96,7 @@ const App = () => {
                             <>
                                 <Route exact path="/admin/register" element={<Register />} />
                                 <Route exact path="/admin/login" element={<Login />} />
-                                <Route path="*" element={<Navigate to="/admin/login" />} />
+                                <Route path="/admin/*" element={<Navigate to="/admin/login" />} />
                             </>
                         )}
                     </>
@@ -109,7 +110,7 @@ const App = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/empleados" element={<Employees />} />
-                <Route path="/217/cerveza-script" element={<ProductsList />} />
+                <Route exact path="/menu/:name" element={<Menu />} />
                 <Route exact path="/editar" element={<EditDatos />} />
                 <Route exact path="/personalizar" element={<Personalizar />} />
                 <Route exact path="/login" element={<Login />} />
